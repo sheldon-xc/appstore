8f3a2ae7e2b78057b94bd3bfbfd2b09de3bfb0f6
Sheldon <sheldon@nmn.im>
2024-09-25 06:46:42 +0800

    feat: add sellerName filter

--- pages/index.vue
+++ pages/index.vue
@@ -24,12 +24,12 @@ const TFAppsObj = JSON.parse(TFApps.value).feed
 
 // 获取推荐的 100 个 App 的 id
 const ids = TFAppsObj.entry.map(item => item.id.attributes['im:id'])
-console.log(ids)
+// console.log(ids)
 
 // 根据 id 获取所有 id 的应用详情
 const { data: lookup } = await useFetch(`${baseUrl}hk/lookup?id=${ids}`)
 const TFAppsList = JSON.parse(lookup.value).results
-console.log(TFAppsList)
+// console.log(TFAppsList)
 
 const searchChange = (val) => {
   searchQuery.value = val
@@ -42,6 +42,7 @@ const AppList = computed(() => {
     return (
       item.trackName.toLowerCase().includes(searchString) ||
       item.description.toLowerCase().includes(searchString) ||
+      item.sellerName.toLowerCase().includes(searchString) ||
       String(item.trackId).includes(searchString)
     )
   })
